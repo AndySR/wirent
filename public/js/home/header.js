7ac704c8d42888462788258b6778e0223eaee3e3
AndySR <yiensuen@gmail.com>
2017-06-16 13:43:52 +1000

    version2.6.6

--- public/js/home/header.js
+++ public/js/home/header.js
@@ -25,8 +25,7 @@
 		.controller('WinCtrl', ['$scope', '$filter','$state','$window', '$http','UserService','readLetters','$localStorage', 'SearchService',function($scope,$filter, $state,$window, $http,UserService,readLetters,$localStorage,SearchService) {
 			$localStorage.headerSetting = {};
 			$scope.name = "Winning";
-			$scope.letternums = 0;
-			
+			$scope.letternums = 0;	
 			$http.get('/customer/profile')
 				.then(function(r) {
 					console.log(r);
